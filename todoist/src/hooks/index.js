c6b6ce606e716a1833dee20f9f6ece4b3d783ae3
Isaque Igor da Silva <isaque.silva@industriall.ai>
2020-04-08 20:54:44 -0300

    Components

--- todoist/src/hooks/index.js
+++ todoist/src/hooks/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import moment from 'moment';
 import { firebase } from '../firebase';
+import { collatedTasksExist } from '../helpers/index';
 
 
 export const useTasks = selectedProject => {
@@ -47,5 +48,31 @@ export const useTasks = selectedProject => {
           return () => unsubscribe();
         }, [selectedProject]);
 
+        return { tasks, archivedTasks };
 }
+
+export const useProjects = () => {
+    const [projects, setProjects] = useState([]);
+  
+    useEffect(() => {
+      firebase
+        .firestore()
+        .collection('projects')
+        .where('userId', '==', 'jlIFXIwyAL3tzHMtzRbw')
+        .orderBy('projectId')
+        .get()
+        .then(snapshot => {
+          const allProjects = snapshot.docs.map(project => ({
+            ...project.data(),
+            docId: project.id,
+          }));
+  
+          if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+            setProjects(allProjects);
+          }
+        });
+    }, [projects]);
+  
+    return { projects, setProjects };
+  };
 
